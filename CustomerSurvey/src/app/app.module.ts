4bf2de44809ca1ca6ea1ba299587a3301fc125a9
harshith.shankar.ks@gmail.com <harshith.k-s@siemens.com>
2023-08-06 21:44:02 +0530

    Added a sample header, a form card, a sample question line and an editfield

--- CustomerSurvey/src/app/app.module.ts
+++ CustomerSurvey/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormComponent } from './form/form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
